7b3177f31c16e3d281a0c1265ed410077667f2e6
Daniel Lugo <gh@danlugo.dev>
2023-06-05 20:04:53 -0500

    v1.0.0

---
+++ index.ts
@@ -0,0 +1,102 @@
+type Unsubscriber = () => void
+
+const identity = <T>(val: T): T => val
+
+export type Subber<T = null> = (val: T | null) => void
+
+export interface Signal<T = null> {
+  readonly current: T
+  readonly set: (newVal: T) => void
+  readonly sub: (subber: Subber<T>) => Unsubscriber
+}
+
+export interface Event {
+  id: number
+  name: string
+}
+
+export type FiringHandler<T = null, Deps = unknown[]> = (
+  current: T,
+  deps: Deps,
+  event?: Event
+) => T
+
+const isEventSignal = (signal: unknown): signal is Signal<Event> => {
+  type _EventSignal = { __IS__EVENT: boolean }
+  return Boolean((signal as unknown as _EventSignal).__IS__EVENT)
+}
+
+export const createSignal = <T = null>(
+  initialValue: T,
+  deps: Signal<unknown>[] = [],
+  onFire: FiringHandler<T> = identity
+): Signal<T> => {
+  let current = initialValue
+  const depCurrents = deps.map((dep) => dep.current)
+
+  let subs: readonly Unsubscriber[] = []
+  const subbers: Subber<T>[] = []
+
+  const notify = () => {
+    subbers.forEach((subber) => {
+      subber(current)
+    })
+  }
+
+  const sub = () => {
+    subs = deps.map((dep, i) =>
+      dep.sub(() => {
+        if (isEventSignal(dep)) {
+          current = onFire(current, deps, dep.current)
+        } else {
+          depCurrents[i] = dep.current
+          current = onFire(current, deps)
+        }
+        notify()
+      })
+    )
+  }
+
+  const unsub = () => {
+    subs.forEach((unsubber) => {
+      unsubber()
+    })
+    subs = []
+  }
+
+  let hasSubbed = false
+
+  return {
+    current: initialValue,
+    sub: (subber) => {
+      if (!hasSubbed) {
+        sub()
+      }
+      subbers.push(subber)
+
+      return () => {
+        subbers.splice(subbers.indexOf(subber), 1)
+        if (subbers.length === 0) {
+          unsub()
+        }
+      }
+    },
+    set: (newVal) => {
+      current = onFire(newVal, deps)
+
+      notify()
+    },
+  }
+}
+
+export const createEventSignal = (eventName: string): Signal<Event | null> => {
+  const signal = createSignal<Event | null>({
+    id: Math.random(),
+    name: eventName,
+  })
+
+  // @ts-expect-error
+  signal.__IS__EVENT
+
+  return signal
+}
